9a74a684489db93795634b67b3c1c8eba1c259b1
Fitorec <chanerec@gmail.com>
2013-02-12 14:09:12 -0600

    Revisando el espacioado

--- tmpl/default.php
+++ tmpl/default.php
@@ -17,41 +17,41 @@ JHtml::_('bootstrap.tooltip');
 JHTML::_('behavior.calendar');
 $doc =& JFactory::getDocument();
 $doc->addScriptDeclaration('window.addEvent(\'domready\', function() {Calendar.setup({
-				inputField     :    "modrsv-calendar",        // ID del input en donde vamos a instanciar el calendario
-				ifFormat       :    "%Y-%m-%d",               // Formato de la Fecha
-				button         :    "modrsv-calendar-ctrl",   // ID del control el cual dispara el calendario
-				align          :    "Tr",                     // Alineación (Top right - Esquina superior Derecha)
-				singleClick    :    true
-				});});'
+	inputField     :    "modrsv-calendar",        // ID del input en donde vamos a instanciar el calendario
+	ifFormat       :    "%Y-%m-%d",               // Formato de la Fecha
+	button         :    "modrsv-calendar-ctrl",   // ID del control el cual dispara el calendario
+	align          :    "Tr",                     // Alineación (Top right - Esquina superior Derecha)
+	singleClick    :    true
+	});});'
 );
 
 ?>
 <form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="recervaciones-form" class="form-inline">
 	
-		<!-- calendar input -->
-		<div id="form-recervaciones-phone" class="control-group">
-			<div class="controls">
-				<div class="input-prepend input-append">
-					<span class="add-on">
-						<i class="icon-calendar tip" title="Fecha"></i>
-						<label for="modrsv-phone" class="element-invisible">Fecha</label>
-					</span>
-					<input id="modrsv-calendar" type="text" name="phone" class="input-small" tabindex="1" size="18" placeholder="Fecha" />
-					<a href="#" id='modrsv-calendar-ctrl' class="btn hasTooltip" title="Inserte la fecha"><i class="icon-question-sign"></i></a>
-				</div>
+	<!-- calendar input -->
+	<div id="form-recervaciones-phone" class="control-group">
+		<div class="controls">
+			<div class="input-prepend input-append">
+				<span class="add-on">
+					<i class="icon-calendar tip" title="Fecha"></i>
+					<label for="modrsv-phone" class="element-invisible">Fecha</label>
+				</span>
+				<input id="modrsv-calendar" type="text" name="phone" class="input-small" tabindex="1" size="18" placeholder="Fecha" />
+				<a href="#" id='modrsv-calendar-ctrl' class="btn hasTooltip" title="Inserte la fecha"><i class="icon-question-sign"></i></a>
 			</div>
 		</div>
+	</div>
 
 	
-		<div id="form-recervaciones-submit" class="control-group">
-			<div class="controls">
-				<button type="submit" tabindex="3" name="Submit" class="btn btn-primary btn">Aceptar</button>
-			</div>
+	<div id="form-recervaciones-submit" class="control-group">
+		<div class="controls">
+			<button type="submit" tabindex="3" name="Submit" class="btn btn-primary btn">Aceptar</button>
 		</div>
-		<?php
-			$usersConfig = JComponentHelper::getParams('com_users');
-			if ($usersConfig->get('allowUserRegistration')) : ?>
-		<?php endif; ?>
+		</div>
+	<?php
+		$usersConfig = JComponentHelper::getParams('com_users');
+		if ($usersConfig->get('allowUserRegistration')) : ?>
+	<?php endif; ?>
 	</div>
 </form>
 <br>
